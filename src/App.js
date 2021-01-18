abf9707da50ba480dc3b01b4f1685c5fd8eb6591
Kadir <kadir.poyraz@outlook.be>
2021-01-18 16:30:07 +0100

    fin exo10

--- src/App.js
+++ src/App.js
@@ -13,15 +13,34 @@ class App extends Component {
 
   changeParagraphe = (e) =>{
     e.target.previousSibling.textContent = "riche, pauvre"
-  }
+  };
+
+  changeInput = (e) =>{
+    const text = document.getElementById("second");
+    const para = document.querySelector("h2");
+    para.innerHTML = text.value;
+  };
+
+  changeArea = (e) =>{
+    e.target.previousSibling.textContent = e.target.value;
+  };
+
 
   render(){
 
     return (
       <div className="App">
         <h1 onClick={(e)=>{this.changeTitle(e, "Exo10")}}>Exercice 10</h1>
+        <h2>Explication</h2>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, quo.</p>
+        
         <button onClick={this.changeParagraphe}>modifier le paragraphe</button>
+        <br/>
+        <br/>
+        <input type="text" name="" id="second"/><button onClick={this.changeInput}>change</button>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        <textarea name="" id="" cols="30" rows="10" onChange={this.changeArea}></textarea>
 
       </div>
     );
